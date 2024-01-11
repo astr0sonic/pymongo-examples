0bfd14dd8460d40ade97b03ee5a84f8cf1571c6c
astrosonic <z.astrosonic@gmail.com>
2024-01-11 16:29:46 +0300

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# MongoDB usage examples using Python
+
+Based on official courses:
+
+* [Connecting to MongoDB in Python](https://learn.mongodb.com/learn/course/connecting-to-mongodb-in-python/)
+
+
+# Start
+
+Create the virtual environment:
+
+```bash
+./create-venv.sh
+```
+
+Start script:
+
+```python
+python script_name.py
+```
---
+++ connection.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+
+load_dotenv()
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI)
+
+try:
+    ok = client.admin.command("ping")
+    print(ok)
+except PyMongoError as e:
+    print("Cannot connect to db:", e)
